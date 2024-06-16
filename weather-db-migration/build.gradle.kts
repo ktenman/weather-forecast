26783a1538f02d636ce7aa21789acb8b06688464
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-06-17 00:14:37 +0300

    Remove e2e test environment configuration from build scripts

--- weather-db-migration/build.gradle.kts
+++ weather-db-migration/build.gradle.kts
@@ -70,24 +70,7 @@ val isE2ETestEnvironmentEnabled = System.getenv("E2E")?.toBoolean() == true
 
 val test by tasks.getting(Test::class) {
     useJUnitPlatform()
-    if (isE2ETestEnvironmentEnabled) {
-        configureE2ETestEnvironment()
-    } else {
-        exclude("**/e2e/**")
-    }
     finalizedBy(":jacocoTestReport")
-}
-
-fun Test.configureE2ETestEnvironment() {
-    include("**/e2e/**")
-    val properties = mutableMapOf(
-        "webdriver.chrome.logfile" to "build/reports/chromedriver.log",
-        "webdriver.chrome.verboseLogging" to "true"
-    )
-    if (project.hasProperty("headless")) {
-        properties["chromeoptions.args"] = "--headless,--no-sandbox,--disable-gpu"
-    }
-    systemProperties.putAll(properties)
 }
 
 val skipJacoco: Boolean = false
