0056a8dec112a1c5eb9d3120a06d17db85f0878b
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-06-22 22:15:40 +0300

    Remove unused configurations and dependencies from build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,50 +23,15 @@ configurations {
     }
 }
 
-configurations {
-    compileOnly {
-        extendsFrom(configurations.annotationProcessor.get())
-    }
-}
-
 repositories {
     mavenCentral()
-}
-
-extra["springCloudVersion"] = "2023.0.2"
-
-dependencies {
-
-}
-
-dependencyManagement {
-    imports {
-        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
-    }
 }
 
 val isE2ETestEnvironmentEnabled = System.getenv("E2E")?.toBoolean() == true
 
 val test by tasks.getting(Test::class) {
     useJUnitPlatform()
-    if (isE2ETestEnvironmentEnabled) {
-        configureE2ETestEnvironment()
-    } else {
-        exclude("**/e2e/**")
-    }
     finalizedBy(":jacocoTestReport")
-}
-
-fun Test.configureE2ETestEnvironment() {
-    include("**/e2e/**")
-    val properties = mutableMapOf(
-        "webdriver.chrome.logfile" to "build/reports/chromedriver.log",
-        "webdriver.chrome.verboseLogging" to "true"
-    )
-    if (project.hasProperty("headless")) {
-        properties["chromeoptions.args"] = "--headless,--no-sandbox,--disable-gpu"
-    }
-    systemProperties.putAll(properties)
 }
 
 val skipJacoco: Boolean = false
