b10ff4b19b6123caa2277ccecf3cc40fb548dc71
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-06-27 19:38:05 +0300

    Refactor weather-component.vue for improved readability and alert handling

--- ui/components/weather-component.vue
+++ ui/components/weather-component.vue
@@ -5,18 +5,12 @@
         <h3>Weather Forecast</h3>
         <form @submit.prevent="fetchWeatherForecast">
           <div class="input-group mb-3">
-            <input
-                id="location"
-                v-model="location"
-                class="form-control"
-                placeholder="Search for location..."
-                type="text"
-            />
+            <input v-model="location" class="form-control" placeholder="Search for location..." type="text"/>
             <button class="btn btn-primary" type="submit">Search</button>
           </div>
         </form>
         <div v-if="weatherForecast && Object.keys(weatherForecast).length > 0">
-          <div v-for="(forecastList, location) in weatherForecast" id="results" :key="location">
+          <div v-for="(forecastList, location) in weatherForecast" :key="location">
             <h5 class="location">{{ location }}</h5>
             <ul>
               <li v-for="forecast in forecastList" :key="forecast.date">
@@ -30,7 +24,7 @@
             No weather forecast data found.
           </div>
         </div>
-        <div v-if="displayAlert" class="mt-3">
+        <div v-if="alertMessage" class="mt-3">
           <div :class="['alert', alertClass]" role="alert">
             {{ alertMessage }}
           </div>
@@ -40,4 +34,27 @@
   </div>
 </template>
 
-<script lang="ts" src="./weather-component.ts"></script>
+<script lang="ts" setup>
+import {computed, ref} from 'vue';
+import {WeatherForecastResponse} from '../models/weather-forecast';
+import {WeatherService} from '../services/weather-service';
+import {AlertType, getAlertBootstrapClass} from '../models/alert-type';
+
+const alertMessage = ref('');
+const alertType = ref<AlertType | null>(null);
+const weatherService = new WeatherService();
+const location = ref('');
+const weatherForecast = ref<WeatherForecastResponse | null>(null);
+
+const fetchWeatherForecast = async () => {
+  try {
+    weatherForecast.value = await weatherService.getWeatherForecast(location.value);
+    alertMessage.value = '';
+  } catch (error) {
+    alertType.value = AlertType.ERROR;
+    alertMessage.value = 'Failed to load weather forecast. Please try again.';
+  }
+};
+
+const alertClass = computed(() => getAlertBootstrapClass(alertType.value));
+</script>
