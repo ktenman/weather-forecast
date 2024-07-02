612a5f371d7642cc52fcad969480c158d3bf2a03
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-07-02 18:29:08 +0300

    Optimize DB migration (#21)
    * Optimize DB migration scripts
    
    * Fix table formatting in initial SQL migration file for consistency
    
    ---------
    
    Co-authored-by: Konstantin Tenman <ktenman@mercans.com>

--- weather-db-migration/src/main/resources/db/migration/V202406160849__create_initial.sql
+++ weather-db-migration/src/main/resources/db/migration/V202406160849__create_initial.sql
@@ -1,26 +1,40 @@
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
 
+CREATE TABLE location
+(
+    id   BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+        UNIQUE,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
 CREATE TABLE weather_forecast
 (
-    id         BIGSERIAL PRIMARY KEY,
-    date       DATE         NOT NULL,
-    location   VARCHAR(255) NOT NULL,
-    created_at TIMESTAMPTZ  NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ  NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE (date, location)
+    id            BIGSERIAL PRIMARY KEY,
+    date          DATE                                               NOT NULL,
+    created_at    TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at    TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    location_id   BIGINT
+        REFERENCES location,
+    provider_name VARCHAR(255)                                       NOT NULL,
+    UNIQUE (date, location_id)
 );
 
 CREATE TABLE weather_forecast_details
 (
     id              BIGSERIAL PRIMARY KEY,
-    forecast_id     BIGINT      NOT NULL REFERENCES weather_forecast (id),
-    temperature_min DECIMAL(5, 2),
-    temperature_max DECIMAL(5, 2),
+    forecast_id     BIGINT                                             NOT NULL
+        REFERENCES weather_forecast,
+    temperature_min NUMERIC(5, 2),
+    temperature_max NUMERIC(5, 2),
     phenomenon      VARCHAR(255),
-    forecast_type   VARCHAR(10) NOT NULL,
-    created_at      TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at      TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    forecast_type   VARCHAR(10)                                        NOT NULL,
+    created_at      TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at      TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
     UNIQUE (forecast_id, forecast_type)
 );
 
-CREATE INDEX idx_weather_forecast_location_trgm ON weather_forecast USING gin (location gin_trgm_ops);
+CREATE INDEX idx_location_name_trgm
+    ON location USING gin (name gin_trgm_ops);
+
